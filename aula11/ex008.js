dd4323ad14fee84a6d5e8a5cd67b24d1a9f21241
LEANDRO MARQUES DA SILVA <leandrostar.01@gmail.com>
2020-05-20 19:52:55 -0300

    Condicoes JS

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {   //condição simples
+    console.log(`Você ultrapassou o limite de velocidade, MUNTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
