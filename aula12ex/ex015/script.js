94b6750025d6a0477daad9de10e212625b770ae4
LEANDRO MARQUES DA SILVA <leandrostar.01@gmail.com>
2020-05-27 16:10:18 -0300

    final exercicio

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,6 +9,46 @@ function verificar() {
   } else {
     var fsex = window.document.getElementsByName("radsex");
     var idade = ano - Number(fano.value);
-    res.innerHTML = `Idade Calculada ${idade} - ${fsex}`;
+    var genero = "";
+
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (fsex[0].checked) {
+      genero = "Homem"
+      if (idade >= 0 && idade < 10) {
+        // Criança
+        img.setAttribute('src', 'img/criancamenino.png')
+      } else if (idade < 21) {
+        // Jovem
+        img.setAttribute('src', 'img/jovemhomem.png')
+      } else if (idade < 50) {
+        // Adulto
+        img.setAttribute('src', 'img/homem.png')
+      } else {
+        // idoso
+        img.setAttribute('src', 'img/idosohomem.png')
+      }
+    } else {
+      genero = "Mulher"
+      if (idade >= 0 && idade < 10) {
+        // Criança
+        img.setAttribute('src', 'img/criancamenina.png')
+      } else if (idade < 21) {
+        // Jovem
+        img.setAttribute('src', 'img/jovemmulher.png')
+      } else if (idade < 50) {
+        // Adulto
+        img.setAttribute('src', 'img/mulher.png')
+      } else {
+        // idoso
+        img.setAttribute('src', 'img/idosomulher.png')
+      }
+
+    }
+
+    res.style.textAlign = 'center' //formatação de estilo pelo JS
+    res.innerHTML = `Detectamos ${genero}, com idade ${idade} anos`;
+    res.appendChild(img)
   }
 }
