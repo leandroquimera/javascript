c63dfc24332249948bf029b9b5200d3e0a09bf3d
LEANDRO MARQUES DA SILVA <leandrostar.01@gmail.com>
2020-06-04 12:12:50 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+/* console.log('Repedição de linha.')
+console.log('Repedição de linha.')
+console.log('Repedição de linha.')
+console.log('Repedição de linha.')
+console.log('Repedição de linha.')
+console.log('Repedição de linha.')
+console.log('Repedição de linha.') */
+
+var contar = 1
+while (contar < 8) {
+    console.log('Repete essa linha 8x')
+    contar++
+}
