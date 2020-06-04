b1e5955f102f17986e73f862d36ac727abbb6e71
LEANDRO MARQUES DA SILVA <leandrostar.01@gmail.com>
2020-06-04 15:40:52 -0300

    aula14

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -7,7 +7,13 @@ console.log('Repedição de linha.')
 console.log('Repedição de linha.') */
 
 var contar = 1
-while (contar < 8) {
-    console.log('Repete essa linha 8x')
-    contar++
+while (contar < 1) {   // com teste no inicio, Se não satisfazer a condição não faz nada e sai fora
+    console.log(`TESTA primeiro a condição, se SATISFAZ repete - ${contar}`)
+    contar += 1
 }
+
+var contadepois = 1
+do {
+    console.log(`Escreve essa linha de qualquer jeito, depois TESTA se SATISFAZ voltar a repetir - ${contadepois}`)
+    contadepois = contadepois + 1
+} while (contadepois < 1);
---
+++ aula14/ambiente.js
@@ -0,0 +1,23 @@
+/* var c = 1
+while (c <= 3) {
+    console.log(c)
+    c = c + 1
+}
+
+var c1 = 1
+do {
+    console.log(c1)
+    c1 += 1
+} while (c1 <= 5);
+
+for (let c2 = 1; c2 <= 7; c2++) {
+    console.log(c2)
+}
+*/
+
+//---------------------------
+console.log(`Vai comecar...`)
+for (var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log(`Fim!!`)
